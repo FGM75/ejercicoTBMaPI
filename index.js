20173855b88c5416182b52e65bb859aeff1429f1
Felipe <fgonzalezm75@gmail.com>
2021-07-02 13:58:35 +0200

    eleccions bus

--- index.js
+++ index.js
@@ -1,26 +1,57 @@
+require("dotenv").config();
 const inquirer = require("inquirer");
+const chalk = require("chalk");
 
+const tipoDeTransporte = async () => {
+  const respuesta = await inquirer.prompt([
+    {
+      name: "eleccion",
+      message: "¿Que tipo de transporte quieres utilizar?",
+      type: "list",
+      choices: [
+        {
+          name: "BUS",
+          value: "bus",
+        },
+        {
+          name: "METRO",
+          value: "metro",
+        },
+      ],
+    },
+  ]);
+  return respuesta;
+};
 
-inquirer.prompt
-    (
-        [
+(async () => {
+  const respuesta = await tipoDeTransporte;
+  console.log(await tipoDeTransporte());
+
+  if (respuesta.eleccion === "bus") {
+    console.log(chalk.yellow());
+  }
+
+  if (respuesta.eleccion === "metro") {
+    const respuestaMetro = async () => {
+      const response = inquirer.prompt([
+        {
+          name: "mensajeMETRO",
+          message: "¿Qué información extra quiere obtener de cada parada?",
+          type: "checkbox",
+          choices: [
             {
-                name: "eleccion",
-                message: "¿Que tipo de transporte quieres utilizar?",
-                type: "list",
-                choices: [
-                    {
-                        name: "BUS",
-                        value: "bus",
-                    },
-                    {
-                        name: "METRO",
-                        value: "metro",
-                    },
-                ]
-
-            }
-        ]
-    ).then((respuestas) => {
-        console.log(respuestas)
-    });
+              name: "Coordenadas",
+              value: "coordenadas",
+            },
+            {
+              name: "Fecha de inauguración",
+              value: "fechaInauguración",
+            },
+          ],
+        },
+      ]);
+      console.log(response);
+    };
+    return respuestaMetro;
+  }
+})();
